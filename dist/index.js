14307c757e43085564e7ce806b6ea200660b4467
David Oyinbo <dev.davexoyinbo@gmail.com>
2024-05-09 20:08:49 +0100

    v1

---
+++ dist/index.js
@@ -0,0 +1,108 @@
+"use strict";
+var store = {
+    circles: [],
+    mousePosition: {
+        x: undefined,
+        y: undefined
+    }
+};
+console.log("This is the index page");
+var Circle = (function () {
+    function Circle(context, x, y, r, dx, dy, color) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.originalRadius = this.r;
+        this.dx = dx;
+        this.dy = dy;
+        this.context = context;
+        this.color = color;
+        this.shouldFill = false;
+    }
+    Circle.prototype.draw = function () {
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        this.context.strokeStyle = this.color;
+        this.context.stroke();
+        this.context.fillStyle = this.shouldFill ? this.color : "transparent";
+        this.context.fill();
+    };
+    Circle.prototype.update = function () {
+        Object.assign(this, {
+            x: this.x + this.dx,
+            y: this.y + this.dy
+        });
+        if ((this.x + this.r) >= this.context.canvas.width) {
+            this.dx = Math.abs(this.dx) * -1;
+        }
+        else if ((this.x - this.r) <= 0) {
+            this.dx = Math.abs(this.dx);
+        }
+        if ((this.y + this.r) >= this.context.canvas.height) {
+            this.dy = Math.abs(this.dy) * -1;
+        }
+        else if ((this.y - this.r) <= 0) {
+            this.dy = Math.abs(this.dy);
+        }
+        var distanceToMouse = store.mousePosition.x === undefined || store.mousePosition.y === undefined ? Infinity : Math.hypot(this.x - store.mousePosition.x, this.y - store.mousePosition.y);
+        if (distanceToMouse < 100 && this.r < 200) {
+            this.r += 1;
+            this.shouldFill = true;
+        }
+        else if (this.r > this.originalRadius) {
+            this.r -= 1;
+            this.shouldFill = false;
+        }
+        else {
+            this.shouldFill = false;
+        }
+        this.draw();
+    };
+    return Circle;
+}());
+var canvas = document.querySelector(".canvas");
+var context = canvas.getContext("2d");
+window.addEventListener("resize", function () { return setCanvasToFullScreen(canvas); });
+setCanvasToFullScreen(canvas);
+function setCanvasSize(canvas, width, height) {
+    canvas.width = width;
+    canvas.height = height;
+}
+function setCanvasToFullScreen(canvas) {
+    setCanvasSize(canvas, window.innerWidth, window.innerHeight);
+}
+function initialize() {
+    for (var i = 0; i < 200; i++) {
+        var r = 10 + Math.floor(Math.random() * 30);
+        var color = "rgba(".concat(Math.floor(Math.random() * 256), ", ").concat(Math.floor(Math.random() * 256), ", ").concat(Math.floor(Math.random() * 256), ", ").concat(0.5 + Math.random() * 0.5, ")");
+        var x = Math.random() * (canvas.width - r * 2) + r;
+        var y = Math.random() * (canvas.height - r * 2) + r;
+        var dx = 1 + (Math.random() - 0.5) * 3;
+        var dy = 1 + (Math.random() - 0.5) * 3;
+        var circle = new Circle(context, x, y, r, dx, dy, color);
+        store.circles.push(circle);
+        circle.draw();
+    }
+}
+function animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < store.circles.length; i++) {
+        store.circles[i].update();
+    }
+    requestAnimationFrame(animate);
+}
+canvas.addEventListener("mousemove", function (event) {
+    Object.assign(store.mousePosition, {
+        x: event.clientX,
+        y: event.clientY,
+    });
+});
+canvas.addEventListener("mouseleave", function (event) {
+    Object.assign(store.mousePosition, {
+        x: undefined,
+        y: undefined,
+    });
+    console.log("mouse leave");
+});
+initialize();
+animate();
